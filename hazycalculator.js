67ce9b0bc96b19b53f2d303a1ce2565bb793c390
CodyPluchak <pbkid21@yahoo.com>
2021-07-27 22:06:34 -0400

    else if broken

--- hazycalculator.js
+++ hazycalculator.js
@@ -6,29 +6,31 @@ function calculate(inputs) {
     for (let i = 0; i < inputs.length; i++){
     if (inputs[i] === null) {
       nums.push (0)
-    }
-    else if(operators.includes(inputs[i])){
+      console.log(i)
+    }else if(operators.includes(inputs[i])){
         operator = inputs[i]
-    }
-    else if(inputs[i] !== '' && inputs[i] !== undefined && inputs[i] === !isNaN(inputs[i])); {
-
+    }else if(inputs[i] !== '' && inputs[i] !== undefined && !isNaN(inputs[i])); {
+        nums.push(inputs[i])
     }
       
 }
+console.log(nums)
+    if (operator.length !== 1 || nums.length !== 2) return NaN
 
 
-switch (any) {
-    case 'add':
-        value = num1 + num2
+
+switch (operator) {
+    case '+':
+        return nums[0] + nums[1]
         break
-    case 'subtract':
-        value = num1 - num2
+    case '-':
+        return nums[0] - nums[1]
         break
-    case 'multiply':
-        value = num1 * num2
+    case '*':
+        return nums[0] * nums[1]
         break
-    case 'divide':
-        value = num1 / num2
+    case '/':
+        return nums[0] / nums[1]
 
 
 
